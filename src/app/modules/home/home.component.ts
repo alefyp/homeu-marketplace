0e24c2213ba452fb0c9b5e9461fb6e92ccc76200
Alefyp <alejndraprz@gmail.com>
2021-07-09 12:05:20 -0500

    Home styles enhanced

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -3,30 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   carouselContent = [
     {
       img: 'https://images.pexels.com/photos/1005058/pexels-photo-1005058.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      title: 'Hola',
-      description: 'Cosas lindas'
     },
     {
       img: 'https://images.pexels.com/photos/298842/pexels-photo-298842.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      title: 'Hola',
-      description: 'Cosas lindas'
     },
     {
       img: 'https://images.pexels.com/photos/584399/living-room-couch-interior-room-584399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      title: 'Hola',
-      description: 'Cosas lindas'
-    }]
+    },
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
